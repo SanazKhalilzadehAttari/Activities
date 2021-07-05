d5c692c570bd3fcf1db453a77048cca471fe5ee0
Sanaz KhalilzadehAttari <khalilzadeh@integrationworks.de>
2021-07-06 00:28:32 +0200

    ADD CRUD in .net

---
+++ client-app/src/app/layout/Navbar.tsx
@@ -0,0 +1,21 @@
+import { memoryUsage } from "process";
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+export default function NavBar(){
+
+    return (
+        <Menu inverted fixed="top">
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo"></img>
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name="Reactivities"></Menu.Item>
+                <Menu.Item>
+                    <Button positive content="Create Activities" />
+                </Menu.Item>
+            </Container>
+        </Menu>
+    )
+}
