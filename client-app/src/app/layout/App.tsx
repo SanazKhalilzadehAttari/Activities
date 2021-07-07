0aa117c363d8cba32da1d021218310eeb15eadf0
Sanaz KhalilzadehAttari <khalilzadeh@integrationworks.de>
2021-07-07 02:17:30 +0200

    Add view Details

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,17 +8,28 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const[activities,setActivities]= useState<Activity[]>([]);
+  const[selectedActivity,setSelectedActivity]= useState<Activity | undefined>(undefined);
   useEffect(()=> {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(Response =>{
-      console.log(Response);
       setActivities(Response.data);
     })
   },[])
+  function handleSelectActivity(id:string){
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+  function handelCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
   return (
     <>
       <NavBar/>
       <Container  style={{marginTop:'7em'}}>
-     <ActivityDashboard activities={activities}></ActivityDashboard>
+     <ActivityDashboard 
+     activities={activities}
+       selectedActivity={selectedActivity}
+       selectActivity={handleSelectActivity}
+       cancelSelectActivity={handelCancelSelectActivity}/>
+    
     </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { Grid, GridColumn, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/Activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityDetail from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
 
 interface Props{
     activities : Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void ;
+    cancelSelectActivity: () => void ;
 }
 
-
-export default function ActivityDashboard({activities}: Props){
+    export default function ActivityDashboard({activities,selectedActivity,selectActivity,
+        cancelSelectActivity}: Props){
     return(
         <Grid>
             <GridColumn width="10">
-                <List>
-                        {activities.map((activity) =>(
-                        <List.Item key={activity.id}>
-                        {activity.title}
-                    </List.Item>
-                    )
-                
-                )}
-                </List>
+                <ActivityList activities={activities} selectActivity={selectActivity}/>
+            </GridColumn>
+            <GridColumn width="6">
+                {selectedActivity &&
+                <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity}/>}
+            <ActivityForm />
             </GridColumn>
         </Grid>
     )
